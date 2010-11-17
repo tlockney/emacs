853ca92d75f320c4e7c31f0b2051621b1c459d3d
Thomas Lockney <thomas@lockney.net>
2010-11-16 23:17:22 -0800

    Removing Stevey's suggested C-w binding - need my kill-region!

--- customizations/keybindings.el
+++ customizations/keybindings.el
@@ -29,6 +29,6 @@
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
 ; delete more easily
-(global-set-key "\C-w" 'backward-kill-word)
+;(global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
