1ad478058df1918fbb76de7b2955daf8d960c162
Thomas Lockney <thomas@lockney.net>
2011-10-01 23:45:52 -0700

    removing packages from git

--- elpa/slime-clj-0.1.6/slime-clj-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; slime-clj-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "slime-clj" "slime-clj.el" (20103 64535))
-;;; Generated autoloads from slime-clj.el
-
-(add-hook 'slime-load-hook (lambda nil (require 'slime-clj) (slime-clj-init)))
-
-;;;***
-
-;;;### (autoloads nil nil ("slime-clj-pkg.el") (20103 64535 898603))
-
-;;;***
-
-(provide 'slime-clj-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; slime-clj-autoloads.el ends here
