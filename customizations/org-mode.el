e0c71cb1720eb77294ae3ba807e06cf3b245bcef
Thomas Lockney <thomas@lockney.net>
2012-10-22 11:57:59 -0700

    Some helpful orgmode stuff.

--- customizations/org-mode.el
+++ customizations/org-mode.el
@@ -80,3 +80,29 @@
    (dot . t)
    (clojure . t)
    ))
+
+(defvar org-journal-file "~/Dropbox/org/journal.org"
+  "Path to OrgMode journal file.")
+(defvar org-journal-date-format "%Y-%m-%d"
+  "Date format string for journal headings.")
+
+(defun org-journal-entry()
+  "Create a new diary entry for today or append to an existing one."
+  (interactive)
+  (switch-to-buffer (find-file org-journal-file))
+  (widen)
+;  (let ((today (format-time-string org-journal-date-format)))
+  (let ((i-search-forward t)(today (format-time-string org-journal-date-format)))
+    (beginning-of-buffer)
+    (unless (org-goto-local-search-headings today nil t)
+            ((lambda ()
+               (org-insert-heading)
+               (insert today)
+               (insert "\n\n \n"))))
+    (beginning-of-buffer)
+    (org-show-entry)
+    (org-narrow-to-subtree)
+    (end-of-buffer)
+    (backward-char 2)
+    (unless (= (current-column) 2)
+            (insert "\n\n "))))
